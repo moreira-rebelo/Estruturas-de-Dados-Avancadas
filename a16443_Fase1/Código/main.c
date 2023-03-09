32f4a71e599710644a1a77f0b0bed9779b22eb13
moreira-rebelo <moreira.rebelo80@gmail.com>
2023-03-09 23:15:32 +0000

    Primeiras alterações ao código do ano anterior

--- a16443_Fase1/Código/main.c
+++ a16443_Fase1/Código/main.c
@@ -16,41 +16,45 @@
 
 int main() {
 //----- DECLARAÇÃO DE VARIÁVEIS ------
-    int aux, i = 0,;
-    char line[100], c = '|', *sp;
-    listaJob *inicio = NULL;
+    int aux, i = 0, idUtilizador = -1, nif, saldo, permissao;
+    char line[100], c = '|', * sp, nome[20];
 
-//----- LÊ A TABELA A PARTIR DE UM FICHEIRO TXT -----
+    listaUtilizadores *inicio = NULL;
+
+//----- LÊ A TABELA  DE UTILIZADORES A PARTIR DE UM FICHEIRO TXT -----
     FILE *ficheiroUtilizadores;
-    ficheiroUtilizadores = fopen("C:/Users/FR/Desktop/EDA1FINAL/tabela.txt", "r");
+    ficheiroUtilizadores = fopen("C:/Users/morei/Desktop/EDA/a16443_Fase1/Codigo/utilizadores.txt", "r");
 
-    if (ficheiroTabela == NULL){
+    if (ficheiroUtilizadores == NULL){
         perror("Erro ao abrir ficheiro!");
         exit(1);
     }
 
-    while (fgets(line, 100, ficheiroTabela) != NULL) {
+    while (fgets(line, 100, ficheiroUtilizadores) != NULL) {
 
-        while (sp != NULL) { //Enquanto não chega ao fim da linha mantém no ciclo
-            if (nOp == -1){ //Se nOp = -1, quer dizer que ainda não foram importadas máquinas, portanto lê desde o inicio da linha
+;        while (sp != NULL) { //Enquanto não chega ao fim do ficheiro mantém no ciclo
+            if (idUtilizador == -1){ //Se idUtilizador = -1, quer dizer que ainda não foram importados utilizadores, portanto lê desde o inicio da linha
                 sp = strtok(line, ","); //Começa a ler desde o início da linha até encontrar a vírgula e guarda no apontador sp
-                nOp = atoi(sp); //Converte o caracter num inteiro e guarda na variável nOp
+                idUtilizador = atoi(sp); //Converte o caracter num inteiro e guarda na variável idUtilizador
             }
             else { //Caso contrário continua a lêr de onde parou
                 sp = strtok(NULL, ",");
-                nOp = atoi(sp);
+                idUtilizador = atoi(sp);
             }
 
             sp = strtok(NULL, ",");
-            machineId = atoi(sp);
+            permissao = atoi(sp);
 
             sp = strtok(NULL, ",");
-            machineT = atoi(sp);
+            strncpy(nome, sp, sizeof(nome)); //Le o nome e guarda na variável nome
 
-            inicio = inserirJobFim(inicio, nOp, machineId, machineT);
+            sp = strtok(NULL, ",");
+            nif = atoi(sp);
+
+            inicio = inserirUtilizadorFim(inicio, idUtilizador, permissao, nome, nif);
         }
     }
-    fclose(ficheiroTabela);
+    fclose(ficheiroUtilizadores);
 
 
     lerListaTMin(inicio, &nOpLista, &machineIdLista, &machineTLista);
